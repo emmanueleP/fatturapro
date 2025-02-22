a2d184efd1eece1d23d5f2387fb75b1888478801
Emmanuele Pani <150518833+emmanueleP@users.noreply.github.com>
2025-02-22 19:35:47 +0100

    push to git the setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+APP = ['main.py']
+DATA_FILES = [
+    ('assets', ['assets/main.png']),
+    ('utils', ['utils/invoice_parser.py', 'utils/p7m_handler.py', 'utils/theme_manager.py']),
+    ('gui', ['gui/main_window.py', 'gui/info_dialog.py', 'gui/settings_dialog.py'])
+]
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'assets/icon.icns',
+    'plist': {
+        'CFBundleName': 'FatturaPro',
+        'CFBundleDisplayName': 'FatturaPro',
+        'CFBundleGetInfoString': "Visualizzatore Fatture Elettroniche",
+        'CFBundleIdentifier': "com.emmanuele.fatturapro",
+        'CFBundleVersion': "1.0.0",
+        'CFBundleShortVersionString': "1.0.0",
+        'NSHumanReadableCopyright': "© 2025 Emmanuele Pani",
+        'NSHighResolutionCapable': True,
+        'LSMinimumSystemVersion': "13.0.0",
+        'LSApplicationCategoryType': 'public.app-category.business'
+    }
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+) 
